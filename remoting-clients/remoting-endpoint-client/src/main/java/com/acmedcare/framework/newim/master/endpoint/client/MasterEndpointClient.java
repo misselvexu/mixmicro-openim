9582e08819ec5fe01fd2af309c1e10cd4f2ef60d
misselvexu <x_vivi@yeah.net>
2018-11-28 09:39:13 +0800

    upgrade server address ;

--- remoting-clients/remoting-endpoint-client/src/main/java/com/acmedcare/framework/newim/master/endpoint/client/MasterEndpointClient.java
+++ remoting-clients/remoting-endpoint-client/src/main/java/com/acmedcare/framework/newim/master/endpoint/client/MasterEndpointClient.java
@@ -6,4 +6,6 @@ package com.acmedcare.framework.newim.master.endpoint.client;
  * @author <a href="mailto:iskp.me@gmail.com">Elve.Xu</a>
  * @version ${project.version} - 21/11/2018.
  */
-public class MasterEndpointClient {}
+public class MasterEndpointClient {
+
+}
