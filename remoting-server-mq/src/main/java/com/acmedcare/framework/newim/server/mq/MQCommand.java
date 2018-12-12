78853e7b80b6f3585b4648a5052ac94fb2a1da85
misselvexu <x_vivi@yeah.net>
2018-12-12 16:28:01 +0800

    DEFINED MQ SERVER COMMANDS ;

---
+++ remoting-server-mq/src/main/java/com/acmedcare/framework/newim/server/mq/MQCommand.java
@@ -0,0 +1,48 @@
+package com.acmedcare.framework.newim.server.mq;
+
+/**
+ * MQ Command Defined Class
+ *
+ * @author <a href="mailto:iskp.me@gmail.com">Elve.Xu</a>
+ * @version ${project.version} - 2018-12-12.
+ */
+public class MQCommand {
+
+  /** 采样段数据协议 */
+  interface SamplingClient {
+
+    /** 握手协议 */
+    int HANDSHAKE = 0x50001;
+
+    /** 注册协议(上线) */
+    int REGISTER = 0x50002;
+
+    /** 注销协议(下线) */
+    int SHUTDOWN = 0x50003;
+
+    /** 拉取订阅主题列表 */
+    int PULL_TOPIC_SUBSCRIBE_MAPPING = 0x50004;
+  }
+
+  /** 监护端数据协议 */
+  interface MonitorClient {
+
+    /** 握手协议 */
+    int HANDSHAKE = 0x60001;
+
+    /** 注册协议(上线) */
+    int REGISTER = 0x60002;
+
+    /** 注销协议(下线) */
+    int SHUTDOWN = 0x60003;
+
+    /** 主题订阅协议 */
+    int TOPIC_SUBSCRIBE = 0x60004;
+
+    /** 撤销主题订阅协议 */
+    int REVOKE_TOPIC_SUBSCRIBE = 0x60005;
+
+    /** 填补数据协议 */
+    int FIX_MESSAGE = 0x60006;
+  }
+}
