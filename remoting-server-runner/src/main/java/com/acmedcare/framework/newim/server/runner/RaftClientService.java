ac862521a1548fe32bd93de7d46e29e04632437f
misselvexu <x_vivi@yeah.net>
2018-12-19 16:15:12 +0800

    FIX MQ SERVER RAFT UID ISSUES;

--- remoting-server-runner/src/main/java/com/acmedcare/framework/newim/server/runner/RaftClientService.java
+++ remoting-server-runner/src/main/java/com/acmedcare/framework/newim/server/runner/RaftClientService.java
@@ -1,8 +1,11 @@
 package com.acmedcare.framework.newim.server.runner;
 
+import com.acmedcare.framework.boot.snowflake.Snowflake;
 import io.atomix.AtomixClient;
 import io.atomix.variables.DistributedLong;
+import io.atomix.variables.DistributedValue;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,11 +19,28 @@ import org.springframework.stereotype.Component;
 public class RaftClientService {
 
   private static final String UNIFORM_MESSAGE_ID_KEY = "mq_message_id_key";
+  private static final String ID_INITED_KEY = "mq_message_id_inited_key";
+  private static volatile AtomicBoolean inited = new AtomicBoolean(false);
   private final AtomixClient atomixClient;
+  private final Snowflake snowflake;
 
   @Autowired
-  public RaftClientService(AtomixClient atomixClient) {
+  public RaftClientService(AtomixClient atomixClient, Snowflake snowflake) {
     this.atomixClient = atomixClient;
+    this.snowflake = snowflake;
+  }
+
+  private boolean inited() {
+    try {
+      DistributedValue<Object> value = atomixClient.getValue(ID_INITED_KEY).get();
+      if (value == null) {
+        return false;
+      } else {
+        return Boolean.parseBoolean(value.toString());
+      }
+    } catch (Exception e) {
+      return false;
+    }
   }
 
   /**
@@ -31,8 +51,15 @@ public class RaftClientService {
    * @throws InterruptedException exception
    */
   public long nextUniformMessageId() throws ExecutionException, InterruptedException {
+
+    if (inited.compareAndSet(false, true)) {
+      if (!inited()) {
+        atomixClient.getLong(UNIFORM_MESSAGE_ID_KEY).get().set(snowflake.nextId()).join();
+        atomixClient.getValue(ID_INITED_KEY).get().set(true);
+      }
+    }
+
     DistributedLong distributedLong = atomixClient.getLong(UNIFORM_MESSAGE_ID_KEY).get();
-    distributedLong.set(1L).join();
-    return distributedLong.incrementAndGet().get();
+    return atomixClient.getLong(UNIFORM_MESSAGE_ID_KEY).get().incrementAndGet().get();
   }
 }
