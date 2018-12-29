3a69993b54bc4c95dfe9d64797cb101d11819f1a
misselvexu <x_vivi@yeah.net>
2018-12-29 15:33:04 +0800

    UPDATE VERSION 2.2.3.BUILD-SNAPSHOT

--- remoting-clients/remoting-socket-clients/src/main/java/com/acmedcare/tiffany/framework/remoting/jlib/Version.java
+++ remoting-clients/remoting-socket-clients/src/main/java/com/acmedcare/tiffany/framework/remoting/jlib/Version.java
@@ -8,6 +8,6 @@ package com.acmedcare.tiffany.framework.remoting.jlib;
  */
 public final class Version {
   public static String get() {
-    return "2.2.1-RC1";
+    return "2.2.3.BUILD-SNAPSHOT";
   }
 }
--- remoting-server-wss/src/main/bin/startup.sh
+++ remoting-server-wss/src/main/bin/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SERVICE_NAME=remoting-server-wss
-SERVICE_VERSION=2.2.2.BUILD-SNAPSHOT
+SERVICE_VERSION=2.2.3.BUILD-SNAPSHOT
 # Copyright 1999-2018 Acmedcare+ Group Holding Ltd.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
